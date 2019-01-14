5f7ed911b7223d67ba06ba09053121e2d506d72b
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-15 00:42:49 +0600

    user model is added

---
+++ api/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userName: { type: String, required: true },
+    userEmail: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    userPassword: { type: String, required: true },
+    userPicture: { type: String, default: 'image.jpg' }
+});
+
+module.exports = mongoose.model('User', userSchema);
