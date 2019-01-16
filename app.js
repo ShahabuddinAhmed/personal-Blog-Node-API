1bcfe326e3906f69dfd5c7255d51fd53686f3c53
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-16 20:52:07 +0600

    all route is added

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const userRoutes = require('./api/routes/users');
-const addressBookRoutes = require('./api/routes/addressbook');
+const userRoutes = require('./api/routes/user');
+const adminRoutes = require('./api/routes/admin');
+const postRoutes = require('./api/routes/post');
+const CommentRoutes = require('./api/routes/comment');
+const replaytRoutes = require('./api/routes/replay');
+const aboutWriterRoutes = require('./api/routes/about-writer');
+const catagoryRoutes = require('./api/routes/catagory');
 
 mongoose.connect('mongodb://localhost:27017/PersonalBlog', {
     useNewUrlParser: true,
@@ -21,7 +26,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/user', userRoutes);
-app.use('/addressbook', addressBookRoutes);
+app.use('/admin', adminRoutes);
+app.use('/post', postRoutes);
+app.use('/comment', CommentRoutes);
+app.use('/replay', replaytRoutes);
+app.use('/about', aboutWriterRoutes);
+app.use('/catagory', catagoryRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
