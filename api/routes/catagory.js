851170715ed7f5b8750deda97cbeeeb8f0a81564
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-16 20:59:06 +0600

    catagory route is added

---
+++ api/routes/catagory.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const checkAdminAuth = require('../middleware/admin-auth');
+const catagoryController = require('../controller/catagory');
+
+router.get('/', catagoryController.getAllCatagory);
+router.post('/create', catagoryController.createCatagory);
+router.patch('/update/:catagoryID', catagoryController.updateCatagory);
+router.delete('/delete/:catagoryID', catagoryController.deteleteCatagory);
+
+module.exports = router;
