f17b6dc8d6513d76b7f00248337e34d62d9cc7ed
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-16 20:58:09 +0600

    admin route is deleted

---
+++ api/routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const checkAdminAuth = require('../middleware/admin-auth');
+const adminController = require('../controller/admin');
+
+router.get('/all', adminController.getAllAdmin);
+router.post('/register', adminController.registerAdmin);
+router.post('/login', adminController.loginAdmin);
+router.get('/:adminID', adminController.getOneAdmin);
+router.patch('update/:adminID', adminController.updateAdmin);
+router.delete('/delete/:adminID', adminController.deteleteAdmin);
+
+module.exports = router;
