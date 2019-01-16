dc8957f80c64e220481027059abd4e23b4feda27
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-16 20:56:01 +0600

    user model is added

--- api/models/user.js
+++ api/models/user.js
@@ -13,4 +13,4 @@ const userSchema = mongoose.Schema({
     userPicture: { type: String, default: 'image.jpg' }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Users', userSchema);
