2880b4bb5cff574b8d71cb65f168fc13f54ec5b9
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-16 20:55:41 +0600

    about-writer model is added

---
+++ api/models/about-writer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const aboutSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    about: { type: String, required: true },
+    picture: { type: String, default: 'image.jpg' }
+});
+
+module.exports = mongoose.model('AboutWriters', aboutSchema);
