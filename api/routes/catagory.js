93880449e030d38ddf4225360eac28e234ff881e
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-18 23:46:09 +0600

    catagory route is updated

--- api/routes/catagory.js
+++ api/routes/catagory.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const checkAdminAuth = require('../middleware/admin-auth');
 const catagoryController = require('../controller/catagory');
 
-router.get('/', catagoryController.getAllCatagory);
+router.get('/all', catagoryController.getAllCatagory);
 router.post('/create', catagoryController.createCatagory);
 router.patch('/update/:catagoryID', catagoryController.updateCatagory);
 router.delete('/delete/:catagoryID', catagoryController.deteleteCatagory);
