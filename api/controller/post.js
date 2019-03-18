5ce1a2631c47f5acc149908d1d802e75435b0605
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-18 22:43:08 +0600

    post comtroller is updated

--- api/controller/post.js
+++ api/controller/post.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Posts = require('../models/post');
+const nl2br  = require('nl2br');
 
 exports.getAllPost = (req, res, next) => {
     Posts.find()
@@ -21,15 +22,80 @@ exports.getAllPost = (req, res, next) => {
     });
 }
 
+exports.getPublishedPost = (req, res, next) => {
+    const id = 'Public';
+    Posts.find({ postType: id })
+    .exec()
+    .then(docs => {
+        if(docs.length >=1) {
+            console.log("From database", docs);
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({
+                message: "Users document is empty."
+            });
+        }
+    })
+    .catch(err => {
+        res.status(200).json({
+            message: err
+        });
+    });
+}
+
+exports.getOnePost = (req, res, next) => {
+    const id = req.params.postID;
+    Posts.findById(id)
+    .exec()
+    .then(doc => {
+        if(doc) {
+            console.log("From database", doc);
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: "No data is found by provided ID"
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+}
+
+exports.getPostType = (req, res, next) => {
+    const id = req.params.postID;
+    Posts.find({ postType: id })
+    .exec()
+    .then(doc => {
+        if(doc) {
+            console.log("From database", doc);
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: "No data is found by provided ID"
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+}
+
 exports.createPost = (req, res, next) => {
     const post = new Posts({
         _id: new mongoose.Types.ObjectId(),
         title: req.body.title,
-        article: req.body.article,
+        article: nl2br(req.body.article, false),
         catagory: req.body.catagory,
         postType: req.body.postType,
         picture: req.body.picture,
-        publishedDate: req.body.publishedDate,
+        publishedDate: req.body.publishedDate
     });
     post
     .save()
@@ -50,12 +116,27 @@ exports.createPost = (req, res, next) => {
 exports.updatePost = (req, res, next) => {
     const id = req.params.postID;
     Posts.update({ _id: id }, { $set: {
-        title: req.body.title,
-        article: req.body.article,
-        catagory: req.body.catagory,
-        postType: req.body.postType,
-        picture: req.body.picture,
-        publishedDate: req.body.publishedDate,
+        picture: req.file.filename
+    }})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "Data is successfully updated"
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.updateOneItem = (req, res, next) => {
+    const id = req.params.postID;
+    Posts.update({ _id: id }, { $set: {
+        postType: req.body.postType
     }})
     .exec()
     .then(result => {
