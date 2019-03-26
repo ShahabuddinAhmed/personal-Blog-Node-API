59979558860eec098fc85ef3e8bfaec48bd71ae5
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-26 22:41:20 +0600

    post route is updated

--- api/routes/post.js
+++ api/routes/post.js
@@ -2,10 +2,28 @@ const express = require('express');
 const router = express.Router();
 const adminCheckAuth = require('../middleware/admin-auth');
 const postController = require('../controller/post');
+const multer = require('multer');
+const dateFormat = require('dateformat');
+const path = require('path');
 
-router.get('/all', postController.getAllPost);
-router.post('/create', postController.createPost);
-router.patch('/update/:postID', postController.updatePost);
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './public/uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, dateFormat('yyyymmddHHMMss').toString() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.get('/all', postController.getPublishedPost);
+router.get('/al', postController.getAllPost);
+router.get('/readingpost/:postID', postController.getOnePost);
+router.get('/post-type/:postID', postController.getPostType);
+router.post('/create', upload.single('picture'), postController.createPost);
+router.patch('/update/:postID', upload.single('picture'), postController.updatePost);
+router.patch('/updateone/:postID', postController.updateOneItem);
 router.delete('/delete/:postID', postController.deteletePost);
 
 module.exports = router;
